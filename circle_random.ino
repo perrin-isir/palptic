88db3bde50a211f533961c7e1ba541d1cc9fc338
Nicolas Perrin-Gilbert <perrin.isir@gmail.com>
2023-02-11 19:02:56 +0100

    Update circle_random.ino

--- circle_random.ino
+++ circle_random.ino
@@ -6,22 +6,24 @@
 #define PINd 10
 #define PINe 12
 
+#define PERIOD_mode1 300
+#define TIMEon_mode1 60
+
+#define PERIOD_mode2 300
+#define TIMEon_mode2 85
+
 const int eepromAddress = 0; // EEPROM address to store boolean value
 static int myPins[] = {PINa, PINb, PINc, PINd, PINe};
 // or, to test a single Pin:
 // static int myPins[] = {PINa};
 const int length = (sizeof(myPins) / sizeof(myPins[0]));
-const int reference_length = 4;
+const int reference_length = 5;
 static int loop_count = 0;
 static bool mode1 = true;
 static bool change_done = false;
 
-#define PERIOD_mode1 500
-#define TIMEon_mode1 50
-#define PAUSE_mode1 20
-#define PERIOD_mode2 400
-#define TIMEon_mode2 80
-#define PAUSE_mode2 20
+#define PAUSE_mode1 0
+#define PAUSE_mode2 0
 
 void setup() {
   // put your setup code here, to run once
@@ -73,7 +75,7 @@ void loop() {
   intervals_off[0] = 0;
   int tvar_off = 0;
   int tvar_step_off = PERIOD - TIMEon - PAUSE;
-  int tvar_delta_off = tvar_step_off * 4 / 10;
+  int tvar_delta_off = tvar_step_off * 3 / 10;
   for (int i=0; i < true_length; i++) {
     tvar_off += tvar_step_off;
     intervals_off[i+1] = tvar_off;
